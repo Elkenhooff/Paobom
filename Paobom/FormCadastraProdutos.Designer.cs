a614e17ebfe07c96e834162269c8068b52d9a3c3
Pedro David <146982890+Elkenhooff@users.noreply.github.com>
2024-03-04 17:27:37 -0300

    Update paobom
    Cadastra produtos

--- Paobom/FormCadastraProdutos.Designer.cs
+++ Paobom/FormCadastraProdutos.Designer.cs
@@ -30,19 +30,24 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCadastraProdutos));
             pictureBox1 = new PictureBox();
-            textBox1 = new TextBox();
+            txtBarra = new TextBox();
             groupBox1 = new GroupBox();
-            comboBox1 = new ComboBox();
+            rTBObservacao = new RichTextBox();
+            label7 = new Label();
+            cBCategoria = new ComboBox();
+            label3 = new Label();
+            cBoxMedida = new ComboBox();
             label6 = new Label();
-            dateTimePicker2 = new DateTimePicker();
-            dateTimePicker1 = new DateTimePicker();
-            maskedTextBox1 = new MaskedTextBox();
-            textBox2 = new TextBox();
+            dTPValidade = new DateTimePicker();
+            dTPFabricacao = new DateTimePicker();
+            mTBoxValor = new MaskedTextBox();
+            txtNome = new TextBox();
             label5 = new Label();
             label4 = new Label();
             lblValor = new Label();
             label2 = new Label();
             label1 = new Label();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             groupBox1.SuspendLayout();
             SuspendLayout();
@@ -58,96 +63,145 @@
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
-            // textBox1
+            // txtBarra
             // 
-            textBox1.Location = new Point(6, 84);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(221, 26);
-            textBox1.TabIndex = 0;
+            txtBarra.Location = new Point(6, 68);
+            txtBarra.Name = "txtBarra";
+            txtBarra.Size = new Size(309, 29);
+            txtBarra.TabIndex = 0;
             // 
             // groupBox1
             // 
-            groupBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            groupBox1.BackColor = Color.Transparent;
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(rTBObservacao);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(cBCategoria);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(cBoxMedida);
             groupBox1.Controls.Add(label6);
-            groupBox1.Controls.Add(dateTimePicker2);
-            groupBox1.Controls.Add(dateTimePicker1);
-            groupBox1.Controls.Add(maskedTextBox1);
-            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(dTPValidade);
+            groupBox1.Controls.Add(dTPFabricacao);
+            groupBox1.Controls.Add(mTBoxValor);
+            groupBox1.Controls.Add(txtNome);
             groupBox1.Controls.Add(label5);
             groupBox1.Controls.Add(label4);
             groupBox1.Controls.Add(lblValor);
             groupBox1.Controls.Add(label2);
             groupBox1.Controls.Add(label1);
-            groupBox1.Controls.Add(textBox1);
-            groupBox1.Font = new Font("Arial", 12F);
+            groupBox1.Controls.Add(txtBarra);
+            groupBox1.Font = new Font("Arial", 14F);
             groupBox1.Location = new Point(12, 194);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(776, 450);
+            groupBox1.Size = new Size(776, 593);
             groupBox1.TabIndex = 1;
             groupBox1.TabStop = false;
             groupBox1.Text = "Cadastro";
             // 
-            // comboBox1
+            // rTBObservacao
             // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Items.AddRange(new object[] { "Unidade", "KG" });
-            comboBox1.Location = new Point(6, 187);
-            comboBox1.MaxLength = 1;
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(221, 26);
-            comboBox1.TabIndex = 11;
-            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            rTBObservacao.Font = new Font("Arial", 12F);
+            rTBObservacao.Location = new Point(7, 427);
+            rTBObservacao.Name = "rTBObservacao";
+            rTBObservacao.Size = new Size(308, 96);
+            rTBObservacao.TabIndex = 15;
+            rTBObservacao.Text = "";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Arial", 12F);
+            label7.Location = new Point(6, 406);
+            label7.Name = "label7";
+            label7.Size = new Size(101, 18);
+            label7.TabIndex = 14;
+            label7.Text = "Observações";
+            // 
+            // cBCategoria
+            // 
+            cBCategoria.DropDownStyle = ComboBoxStyle.DropDownList;
+            cBCategoria.FormattingEnabled = true;
+            cBCategoria.Items.AddRange(new object[] { "Alimentos", "Bebidas", "Produto de Limpeza" });
+            cBCategoria.Location = new Point(6, 377);
+            cBCategoria.MaxLength = 1;
+            cBCategoria.Name = "cBCategoria";
+            cBCategoria.Size = new Size(309, 30);
+            cBCategoria.TabIndex = 13;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 12F);
+            label3.Location = new Point(6, 356);
+            label3.Name = "label3";
+            label3.Size = new Size(78, 18);
+            label3.TabIndex = 12;
+            label3.Text = "Categoria";
+            // 
+            // cBoxMedida
+            // 
+            cBoxMedida.DropDownStyle = ComboBoxStyle.DropDownList;
+            cBoxMedida.FormattingEnabled = true;
+            cBoxMedida.Items.AddRange(new object[] { "Unidade", "KG" });
+            cBoxMedida.Location = new Point(6, 171);
+            cBoxMedida.MaxLength = 1;
+            cBoxMedida.Name = "cBoxMedida";
+            cBoxMedida.Size = new Size(309, 30);
+            cBoxMedida.TabIndex = 11;
+            cBoxMedida.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
             // 
             // label6
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Arial", 12F);
-            label6.Location = new Point(7, 166);
+            label6.Location = new Point(7, 150);
             label6.Name = "label6";
             label6.Size = new Size(146, 18);
             label6.TabIndex = 10;
             label6.Text = "Unidade de Medida";
             // 
-            // dateTimePicker2
+            // dTPValidade
             // 
-            dateTimePicker2.CalendarFont = new Font("Arial", 12F);
-            dateTimePicker2.Format = DateTimePickerFormat.Short;
-            dateTimePicker2.Location = new Point(6, 343);
-            dateTimePicker2.Name = "dateTimePicker2";
-            dateTimePicker2.Size = new Size(221, 26);
-            dateTimePicker2.TabIndex = 9;
+            dTPValidade.CalendarFont = new Font("Arial", 12F);
+            dTPValidade.Font = new Font("Arial", 12F);
+            dTPValidade.Format = DateTimePickerFormat.Short;
+            dTPValidade.Location = new Point(6, 327);
+            dTPValidade.Name = "dTPValidade";
+            dTPValidade.Size = new Size(309, 26);
+            dTPValidade.TabIndex = 9;
             // 
-            // dateTimePicker1
+            // dTPFabricacao
             // 
-            dateTimePicker1.CalendarFont = new Font("Arial", 12F);
-            dateTimePicker1.Format = DateTimePickerFormat.Short;
-            dateTimePicker1.Location = new Point(6, 293);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(221, 26);
-            dateTimePicker1.TabIndex = 8;
+            dTPFabricacao.CalendarFont = new Font("Arial", 12F);
+            dTPFabricacao.Font = new Font("Arial", 12F);
+            dTPFabricacao.Format = DateTimePickerFormat.Short;
+            dTPFabricacao.Location = new Point(6, 277);
+            dTPFabricacao.Name = "dTPFabricacao";
+            dTPFabricacao.Size = new Size(309, 26);
+            dTPFabricacao.TabIndex = 8;
             // 
-            // maskedTextBox1
+            // mTBoxValor
             // 
-            maskedTextBox1.Font = new Font("Arial", 12F);
-            maskedTextBox1.Location = new Point(6, 240);
-            maskedTextBox1.Mask = "$ 000.00";
-            maskedTextBox1.Name = "maskedTextBox1";
-            maskedTextBox1.Size = new Size(221, 26);
-            maskedTextBox1.TabIndex = 7;
+            mTBoxValor.Font = new Font("Arial", 12F);
+            mTBoxValor.Location = new Point(6, 224);
+            mTBoxValor.Mask = "$ 000.00";
+            mTBoxValor.Name = "mTBoxValor";
+            mTBoxValor.Size = new Size(309, 26);
+            mTBoxValor.TabIndex = 7;
             // 
-            // textBox2
+            // txtNome
             // 
-            textBox2.Location = new Point(6, 137);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(221, 26);
-            textBox2.TabIndex = 6;
+            txtNome.Location = new Point(6, 121);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(309, 29);
+            txtNome.TabIndex = 6;
             // 
             // label5
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Arial", 12F);
-            label5.Location = new Point(6, 322);
+            label5.Location = new Point(6, 306);
             label5.Name = "label5";
             label5.Size = new Size(130, 18);
             label5.TabIndex = 5;
@@ -157,7 +211,7 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Arial", 12F);
-            label4.Location = new Point(6, 272);
+            label4.Location = new Point(6, 256);
             label4.Name = "label4";
             label4.Size = new Size(148, 18);
             label4.TabIndex = 4;
@@ -167,7 +221,7 @@
             // 
             lblValor.AutoSize = true;
             lblValor.Font = new Font("Arial", 12F);
-            lblValor.Location = new Point(6, 219);
+            lblValor.Location = new Point(6, 203);
             lblValor.Name = "lblValor";
             lblValor.Size = new Size(44, 18);
             lblValor.TabIndex = 3;
@@ -177,7 +231,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Arial", 12F);
-            label2.Location = new Point(6, 116);
+            label2.Location = new Point(6, 100);
             label2.Name = "label2";
             label2.Size = new Size(131, 18);
             label2.TabIndex = 2;
@@ -187,18 +241,29 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Arial", 12F);
-            label1.Location = new Point(6, 63);
+            label1.Location = new Point(6, 47);
             label1.Name = "label1";
             label1.Size = new Size(125, 18);
             label1.TabIndex = 1;
             label1.Text = "Código de Barra";
             // 
+            // button1
+            // 
+            button1.Location = new Point(331, 208);
+            button1.Name = "button1";
+            button1.Size = new Size(263, 116);
+            button1.TabIndex = 16;
+            button1.Text = "Testar o banco de dados.";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
             // FormCadastraProdutos
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 192, 128);
             BackgroundImageLayout = ImageLayout.Zoom;
-            ClientSize = new Size(800, 622);
+            ClientSize = new Size(800, 765);
             Controls.Add(groupBox1);
             Controls.Add(pictureBox1);
             FormBorderStyle = FormBorderStyle.Fixed3D;
@@ -216,18 +281,23 @@
         #endregion
 
         private PictureBox pictureBox1;
-        private TextBox textBox1;
+        private TextBox txtBarra;
         private GroupBox groupBox1;
-        private TextBox textBox2;
+        private TextBox txtNome;
         private Label label5;
         private Label label4;
         private Label lblValor;
         private Label label2;
         private Label label1;
-        private MaskedTextBox maskedTextBox1;
-        private DateTimePicker dateTimePicker1;
-        private DateTimePicker dateTimePicker2;
-        private ComboBox comboBox1;
+        private MaskedTextBox mTBoxValor;
+        private DateTimePicker dTPFabricacao;
+        private DateTimePicker dTPValidade;
+        private ComboBox cBoxMedida;
         private Label label6;
+        private ComboBox cBCategoria;
+        private Label label3;
+        private RichTextBox rTBObservacao;
+        private Label label7;
+        private Button button1;
     }
 }
--- Paobom/FormCadastraProdutos.cs
+++ Paobom/FormCadastraProdutos.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,17 +16,51 @@ namespace Paobom
         public FormCadastraProdutos()
         {
             InitializeComponent();
-           // comboBox1.
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (comboBox1.SelectedIndex == 0)
+            if (cBoxMedida.SelectedIndex == 1)
+            {
+                lblValor.Text = "Valor (100g)";
+            }
+            else
             {
                 lblValor.Text = "Valor";
             }
-            else if (comboBox1.SelectedIndex == 1) {
-                lblValor.Text = "Valor (100g)";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //MessageBox.Show(mTBoxValor.Text.Substring(3), "teste", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                SqlConnection conexao = new SqlConnection(BD.StringConexao);
+                conexao.Open();
+
+                string sql = "INSERT INTO produtos(pro_codigo, pro_nome, pro_medida, pro_valor, pro_fabricacao, pro_validade, pro_categoria, pro_observacao) VALUES (@codigo, @nome, @medida, @valor, @fabricacao, @validade, @categoria, @observacao);";
+
+                SqlCommand comando = new SqlCommand(sql, conexao);
+
+                comando.Parameters.Add("@codigo", SqlDbType.Int).Value = Convert.ToInt32(txtBarra.Text);
+                comando.Parameters.Add("@nome", SqlDbType.VarChar).Value = txtNome.Text;
+                comando.Parameters.Add("@medida", SqlDbType.VarChar).Value = cBoxMedida.Text;
+                comando.Parameters.Add("@valor", SqlDbType.Decimal).Value = Convert.ToDecimal(mTBoxValor.Text.Substring(3));
+                comando.Parameters.Add("@fabricacao", SqlDbType.Date).Value = Convert.ToDateTime(dTPFabricacao.Text);
+                comando.Parameters.Add("@validade", SqlDbType.Date).Value = Convert.ToDateTime(dTPValidade.Text);
+                comando.Parameters.Add("@categoria", SqlDbType.VarChar).Value = cBCategoria.Text;
+                comando.Parameters.Add("@observacao", SqlDbType.VarChar).Value = rTBObservacao.Text;
+
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Produto inserido com sucesso", "Paobom", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                conexao.Close();
+
+            }
+            catch (Exception a)
+            {
+                MessageBox.Show(a.Message, "Erro no banco de dados", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
             }
         }
     }
